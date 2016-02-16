479b6619c0030fd71d4a20f4a872f0ccfa0f121a
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-02-16 23:08:18 +0100

    Change configuration

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var express = require('express');
-exports.router = express.Router();
-/* GET home page. */
-exports.router.get('/', (req, res, next) => {
-    res.render('index', {
-        title: 'Express'
-    });
-});
